abc8522237a520e0b0857259136e8cb9d35bb9de
Anirban <bloodraven166@gmail.com>
2022-08-17 23:04:42 -0700

    Added another rule, used scenarios, made the code and nomenclature fall into more of a sensible example (in a simple form, with basic arithmetic ops being rules), and ensured that it works (stuff is seeded though)

--- test.cpp
+++ test.cpp
@@ -7,29 +7,59 @@
 
 typedef double D;
 typedef uint32_t U32;
+#define DRANGE 100000
 
-class ExampleClass { public: D ExampleFunction(D a, D b);  };
-D ExampleClass::ExampleFunction(D a, D b) { return (a / b); }
+class ArithmeticOperations          
+{ 
+  public: 
+    D DivisionFunction(D a, D b);          // Division by zero check
+    U32 AdditionFunction(U32 a, U32 b);    // U32 overflow check
+};
+D ArithmeticOperations::DivisionFunction(D a, D b)       { return (a / b); }
+U32 ArithmeticOperations::AdditionFunction(U32 a, U32 b) { return (a + b); }
 
-struct ExampleRule: public STest::Rule<ExampleClass> 
+struct Division: public STest::Rule<ArithmeticOperations> 
 {
-    ExampleRule(const char* ruleName);
-    bool precondition(const ExampleClass& state);
-    void action(ExampleClass& truth);
+    Division(const char* ruleName);
+    bool precondition(const ArithmeticOperations& state);
+    void action(ArithmeticOperations& truth);
 };
 
-ExampleRule::ExampleRule(const char* ruleName): STest::Rule<ExampleClass>(ruleName) {}
-bool ExampleRule::precondition(const ExampleClass &state) { return true; }
-void ExampleRule::action(ExampleClass &state) 
+Division::Division(const char* ruleName): STest::Rule<ArithmeticOperations>(ruleName) {}
+bool Division::precondition(const ArithmeticOperations &state) { return true; }
+void Division::action(ArithmeticOperations &state) 
 {
-    D dividend = STest::Pick::lowerUpper(0, 100), divisor = STest::Pick::lowerUpper(0, 100);
-    D result = state.ExampleFunction(dividend, divisor);
+    D dividend = STest::Pick::lowerUpper(0, DRANGE), divisor = STest::Pick::lowerUpper(0, DRANGE);
+    D result = state.DivisionFunction(dividend, divisor);
     std::cout << "Result of division: " << dividend << "/" << divisor << " = " << result << "\n";
+}
+
+struct Addition: public STest::Rule<ArithmeticOperations> 
+{
+    Addition(const char* ruleName);
+    bool precondition(const ArithmeticOperations& state);
+    void action(ArithmeticOperations& truth);
+};
+
+Addition::Addition(const char* ruleName): STest::Rule<ArithmeticOperations>(ruleName) {}
+bool Addition::precondition(const ArithmeticOperations &state) { return true; }
+void Addition::action(ArithmeticOperations &state) 
+{
+    U32 firstU32 = STest::Pick::lowerUpper(0, UINT32_MAX), secondU32 = STest::Pick::lowerUpper(0, UINT32_MAX);
+    U32 result = state.AdditionFunction(firstU32, secondU32);
+    std::cout << "Result of addition: " << firstU32 << "+" << secondU32 << " = " << result << "\n";
 }
 
 int main()
 {
-    ExampleClass exampleClassObject;
-    ExampleRule exampleRuleObject("ExampleRule");
-    exampleRuleObject.apply(exampleClassObject); 
-}    
+    ArithmeticOperations exampleObject;
+
+    Division exampleDivisionRuleObject("Division");
+    Addition exampleAdditionRuleObject("Addition");
+    exampleDivisionRuleObject.apply(exampleObject);
+    exampleAdditionRuleObject.apply(exampleObject);
+
+    STest::Rule<ArithmeticOperations>* rules[] = { &exampleDivisionRuleObject, &exampleAdditionRuleObject };
+    STest::RandomScenario<ArithmeticOperations> randomRules("Peanuts", rules, 2);
+    STest::BoundedScenario<ArithmeticOperations> bounded("NASA Jesus", randomRules, 42069);
+}
