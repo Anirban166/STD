6200d79edc1fedf9365305b8481224240153202c
Anirban <bloodraven166@gmail.com>
2022-08-20 18:10:35 -0700

    Added the DeepState harness version of fuzztest.cpp

---
+++ Fuzz/DeepState/fuzztest_deepstateharness.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "../../STest/Rule/Rule.hpp"
+#include "../../STest/Pick/Pick.hpp"
+#include <deepstate/DeepState.hpp>
+
+//typedef uint32_t UINT32;
+using namespace deepstate;
+
+class ArithmeticOperations  { public: DEEPSTATE_NOINLINE float DivisionFunction(int a, int b); };
+DEEPSTATE_NOINLINE float ArithmeticOperations::DivisionFunction(int a, int b) { return (a / b); }
+
+struct Division: public STest::Rule<ArithmeticOperations> 
+{
+    Division(const char* ruleName);
+    bool precondition(const ArithmeticOperations& state);
+    void action(ArithmeticOperations& truth);
+};
+
+Division::Division(const char* ruleName): STest::Rule<ArithmeticOperations>(ruleName) {}
+bool Division::precondition(const ArithmeticOperations &state) { return true; }
+void Division::action(ArithmeticOperations &state) 
+{
+    uint32_t dividend = DeepState_UIntInRange(0, 100), divisor  = DeepState_UIntInRange(0, 100);   
+    ASSERT_NE(divisor, 0) 
+    << "Divisor cannot be 0! (divisor and dividend values for this run: " << dividend << ", " << divisor << ")";    
+    float result = state.DivisionFunction(dividend, divisor);   
+    // Not really concerned about the result if it's a successful case (divisor!=0), but gotta keep the compiler happy with its' unusued variable warning:
+    (void)result;
+}
+
+TEST(ArithmeticOps, DivisionByZero) 
+{
+    ArithmeticOperations arithmeticOpsObject;
+    Division divisionRuleObject("Division");
+    divisionRuleObject.apply(arithmeticOpsObject);
+}
